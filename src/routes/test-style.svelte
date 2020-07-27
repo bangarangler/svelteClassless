8adf6192f7117abaad942de73e30e59da18619c2
Jon <bangarangler@gmail.com>
2020-07-26 22:54:30 -0400

    added some default styling for blockquote element and a inside of p, along with a inside of blockquote

--- src/routes/test-style.svelte
+++ src/routes/test-style.svelte
@@ -27,7 +27,7 @@ Lorem ipsum dolor sit amet, <a href="/about">consectetur</a> adipiscing elit, se
 <p class="stylish-small">This text should be supper pretty</p>
 
 <blockquote> 
-Lorem ipsum dolor sit amet, <strong>consectetur</strong> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Lorem ipsum dolor sit amet, <strong>consectetur</strong> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation <a href="/about">consectetur</a> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </blockquote>
 <hr />
 
