062247a69de010f2cf289b395bfb26265abcb854
Jon <bangarangler@gmail.com>
2020-07-26 22:38:56 -0400

    adding in typography, css variables, starting to build out default element styles

---
+++ src/routes/test-style.svelte
@@ -0,0 +1,60 @@
+<style>
+  .stylish {
+    font-family: var(--slantText);
+    font-size: var(--h3);
+  }
+  .stylish-small {
+    font-family: var(--slantText);
+    font-size: var(--smallTextSize);
+  }
+  
+</style>
+
+<h1>Test Classless Styles</h1>
+
+<p>
+Lorem ipsum dolor sit amet, <a href="/about">consectetur</a> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+<h2>Test Classless Styles</h2>
+<h3>Hello World</h3>
+<h4>Hello World</h4>
+<h5>Hello World</h5>
+<h6>Hello World</h6>
+
+<hr />
+
+<p class="stylish-small">This text should be supper pretty</p>
+
+<blockquote> 
+Lorem ipsum dolor sit amet, <strong>consectetur</strong> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</blockquote>
+<hr />
+
+<p class="stylish">This text should be supper pretty</p>
+
+<ul>
+  <li>item here</li>
+  <li>another item here</li>
+  <li>last item for now</li>
+</ul>
+
+<div>
+  <label for="text">Text</label>
+  <input id="text" type="text"/>
+</div>
+<div>
+  <label for="number">Number</label>
+  <input id="number" type="number"/>
+</div>
+<div>
+  <label for="email">Email</label>
+  <input id="email" type="email"/>
+</div>
+<select>
+  <option>One</option>
+  <option>Two</option>
+  <option>Three</option>
+</select>
+<button>Submit</button>
+<button>Cancel</button>
